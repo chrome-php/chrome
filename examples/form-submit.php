988577d2a084dc366cbf13a6400c1fd279e13a3f
Soufiane Ghzal <sghzal@gmail.com>
2018-04-26 12:48:20 +0200

    Update form-submit.php

--- examples/form-submit.php
+++ examples/form-submit.php
@@ -13,12 +13,15 @@ $page = $browser->createPage();
 $page->navigate('file://' . __DIR__ . '/html/form.html')->waitForNavigation();
 
 // put 'hello' in the input and submit the form
-$page->evaluate(
+$evaluation = $page->evaluate(
 '(() => {
         document.querySelector("#myinput").value = "hello";
         document.querySelector("#myform").submit();
     })()'
-)->waitForPageReload();
+);
+
+// wait for the page to be reloaded
+$evaluation->waitForPageReload();
 
 // get value in the new page
 $value = $page->evaluate('document.querySelector("#value").innerHTML')->getReturnValue();
