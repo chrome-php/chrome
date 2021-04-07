be2d38411833398e2f0027d2176d43d9a3191cae
momala454 <madri2@gmail.com>
2021-04-07 18:09:33 +0200

    Add parameter captureBeyondViewport to screenshot
    New parameter to allow to capture screenshot beyond the viewport without needing to resize it.
    
    ```php
    $page->screenshot(['captureBeyondViewport' => true, 'clip' => $page->getFullPageClip()])```

--- src/Page.php
+++ src/Page.php
@@ -423,6 +423,7 @@ class Page
      *  - format: "png"|"jpg" default "png"
      *  - quality: number from 0 to 100. Only for jpegs
      *  - clip: instance of a Clip to choose an area for the screenshot
+     *  - captureBeyondViewport: Boolean. capture the screenshot beyond the viewport. Defaults to false
      *
      * @return PageScreenshot
      * @throws CommunicationException
@@ -433,6 +434,10 @@ class Page
 
         $screenshotOptions = [];
 
+        if (array_key_exists('captureBeyondViewport', $options)) {
+            $screenshotOptions['captureBeyondViewport'] = $options['captureBeyondViewport'];
+        }        
+        
         // get format
         if (array_key_exists('format', $options)) {
             $screenshotOptions['format'] = $options['format'];
