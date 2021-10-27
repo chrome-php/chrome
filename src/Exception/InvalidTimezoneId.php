37b3c1801df893b0cbdafad4bbc80c4e8eaa6d8c
Vaggelis Yfantis <me@octoper.me>
2021-10-27 23:50:27 +0300

    Add HeadlessChromium\Page::setTimezone method

---
+++ src/Exception/InvalidTimezoneId.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * This file is part of Chrome PHP.
+ *
+ * (c) Soufiane Ghzal <sghzal@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace HeadlessChromium\Exception;
+
+class InvalidTimezoneId extends \Exception
+{
+}
