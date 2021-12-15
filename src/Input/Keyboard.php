d2f5d8111b6428a068b7b0d57391cdade240e172
Yandiev Ruslan <yandievruslan@gmail.com>
2021-12-15 17:20:02 +0300

    Update Keyboard.php

--- src/Input/Keyboard.php
+++ src/Input/Keyboard.php
@@ -56,7 +56,7 @@ class Keyboard
             $this->page->getSession()->sendMessageSync(new Message('Input.dispatchKeyEvent', [
                 'type' => 'char',
                 'modifiers' => $this->getModifiers(),
-                'text' => mb_substr($text, $i, 1),
+                'text' => \mb_substr($text, $i, 1),
             ]));
 
             \usleep($this->sleep);
