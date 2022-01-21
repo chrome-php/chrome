ea49b2a1624e2726788f6f73409bf49135e778c9
Pavel Karfik <pavel.karfik@cdn77.com>
2022-01-21 17:16:39 +0100

    Add test

--- tests/PageTest.php
+++ tests/PageTest.php
@@ -414,4 +414,18 @@ class PageTest extends BaseTestCase
 
         $this->assertStringContainsString('<h1>bar</h1>', $page->getHtml());
     }
+
+    public function testWaitUntilContains():void
+    {
+        $factory = new BrowserFactory();
+
+        $browser = $factory->createBrowser();
+        $page = $browser->createPage();
+
+        $page->navigate(self::sitePath('elementLoad.html'))->waitForNavigation();
+
+        $page->waitUntilContains('div[data-name=\"el\"]');
+
+        $this->assertStringContainsString('<div data-name="el"></div>', $page->getHtml());
+    }
 }
