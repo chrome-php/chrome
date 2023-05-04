dffb0f79c489d6e5f7e365e573deb6ad46d37625
divinity76 <divinity76@gmail.com>
2023-05-04 13:27:58 +0200

    remove comments
    requested at https://github.com/chrome-php/chrome/pull/516#issuecomment-1533022892

--- src/Page.php
+++ src/Page.php
@@ -894,10 +894,7 @@ class Page
             return $this->evaluate('document.documentElement.outerHTML')->getReturnValue($timeout);
         } catch (JavascriptException $e) {
             if (0 === \strpos($e->getMessage(), 'Error during javascript evaluation: TypeError: Cannot read properties of null (reading \'outerHTML\')')) {
-                // sometimes after a page reload, for a split second,
-                // document.documentElement does not exist
-                // (not sure if its a chromium bug or intentional but either way)
-                \usleep(1000); // 1ms seems to be more than enough, unable to reproduce.
+                \usleep(1000);
 
                 return $this->evaluate('document.documentElement.outerHTML')->getReturnValue($timeout);
             }
