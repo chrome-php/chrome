a3550467c8dc42ca6df39dc6c65b3e743f443be3
Maarten Buis <buismaarten@outlook.com>
2023-05-27 16:15:53 +0200

    Update Utils.php

--- src/Utils.php
+++ src/Utils.php
@@ -15,7 +15,6 @@ use HeadlessChromium\Communication\Connection;
 use HeadlessChromium\Communication\Message;
 use HeadlessChromium\Dom\Selector\Selector;
 use HeadlessChromium\Exception\CommunicationException;
-use HeadlessChromium\Exception\JavascriptException;
 use HeadlessChromium\Exception\OperationTimedOut;
 
 class Utils
