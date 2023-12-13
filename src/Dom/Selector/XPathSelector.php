c1e03917ff1f66f4d62ba085d17d63563d2ff645
divinity76 <hans@loltek.net>
2023-12-13 11:37:27 +0100

    XPathSelector::quote(string $str): string
    method to quote strings in XPath, similar to PDO::quote()
    
    Quoting strings in XPath is surprisingly difficult to do, in the edge case where the string contains both double-quotes and single-quotes. Got the algorithm from https://stackoverflow.com/a/1352556/1067003

--- src/Dom/Selector/XPathSelector.php
+++ src/Dom/Selector/XPathSelector.php
@@ -33,4 +33,47 @@ final class XPathSelector implements Selector
             $position
         );
     }
+
+    /**
+     * Quotes a string for use in an XPath expression.
+     * 
+     * Example: new XPathSelector("//span[contains(text()," . XPathSelector::quote($string) . ")]")
+     * 
+     * @author  Robert Rossney ( https://stackoverflow.com/users/19403/robert-rossney )
+     * @param string $value
+     * @return string
+     */
+    public static function quote(string $value): string
+    {
+        if (false === strpos($value, '"')) {
+            return '"' . $value . '"';
+        }
+        if (false === strpos($value, '\'')) {
+            return '\'' . $value . '\'';
+        }
+        // if the value contains both single and double quotes, construct an
+        // expression that concatenates all non-double-quote substrings with
+        // the quotes, e.g.:
+        //    concat("'foo'", '"', "bar")
+        $sb = 'concat(';
+        $substrings = explode('"', $value);
+        for ($i = 0; $i < count($substrings); ++$i) {
+            $needComma = ($i > 0);
+            if ($substrings[$i] !== '') {
+                if ($i > 0) {
+                    $sb .= ', ';
+                }
+                $sb .= '"' . $substrings[$i] . '"';
+                $needComma = true;
+            }
+            if ($i < (count($substrings) - 1)) {
+                if ($needComma) {
+                    $sb .= ', ';
+                }
+                $sb .= "'\"'";
+            }
+        }
+        $sb .= ')';
+        return $sb;
+    }
 }
