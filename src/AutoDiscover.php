46fd07449a3b7348eee7a5bf7bc1706fffb59d40
divinity76 <divinity76@gmail.com>
2024-01-11 20:51:23 +0100

    prefer chromium-browser over chromium
    there is also a FOSS game by the name chromium iirc..

--- src/AutoDiscover.php
+++ src/AutoDiscover.php
@@ -40,7 +40,7 @@ class AutoDiscover
             case 'Windows':
                 return self::getFromRegistry() ?? '%ProgramFiles(x86)%\Google\Chrome\Application\chrome.exe';
             default:
-                return explode("\n", (string)self::shellExec("bash -c 'command -v google-chrome chrome chromium chromium-browser'"), 2)[0] ?: 'chrome';
+                return explode("\n", (string)self::shellExec("bash -c 'command -v google-chrome chrome chromium-browser chromium'"), 2)[0] ?: 'chrome';
         }
     }
 
