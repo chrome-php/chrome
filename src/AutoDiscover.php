496da1bec75b4f20ccfd9492230e291e4ec810bc
divinity76 <hans@loltek.net>
2024-03-19 18:08:42 +0100

    fsck

--- src/AutoDiscover.php
+++ src/AutoDiscover.php
@@ -54,6 +54,7 @@ class AutoDiscover
                         }
                     }
                 }
+
                 return 'chrome'; // ... very unlikely to actually work, but this retains the original behavior..
                 // throw new \RuntimeException('Could not find chrome binary'); // this makes more sense tbh
         }
